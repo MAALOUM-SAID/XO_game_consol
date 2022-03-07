55facb703dd8c0e31b9c5c522cc67fdabbeec6b4
MAALOUM-SAID <93826377+MAALOUM-SAID@users.noreply.github.com>
2022-03-07 22:02:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from auto import auto_player
+import termcolor as tc
 from multi_players import two_players
 def table_game(a=1,b=2,c=3,d=4,e=5,f=6,i=7,g=8,k=9):
     return "\033[1;33m"+f"""
@@ -12,17 +13,17 @@ def table_game(a=1,b=2,c=3,d=4,e=5,f=6,i=7,g=8,k=9):
         +-------+-------+-------+
     """
 def head_game():
-    print("\033[1;32m"+"\n\t\tWelcom to X|O Game \nEnter the number of the position you want to play in")
+    print(tc.colored("\n\t\tWelcom to X|O Game \nEnter the number of the position you want to play in",color="green"))
     print(table_game())
-    print("Game Starting ..... ",end=" ")
+    print(tc.colored("Game Starting ..... ",color="red"),end=" ")
 head_game()
-choice=input("\nif you want to play with your friend enter Y or N if you want to play with auto play : ")
-symbol= input("X or O : ")
+choice=input(tc.colored("\nif you want to play with your friend enter Y or N if you want to play with auto play: ",color="blue"))
+symbol=input(f" {tc.colored('X',color='green')} or {tc.colored('O',color='red')} : ")
 if symbol.upper() == "X":
-    auto_symbol = "O"
+    auto_symbol =tc.colored("O",color="red")
 else:
-    auto_symbol = "X"
+    auto_symbol = tc.colored("X",color="yellow")
 if choice.lower()=="y":
-    two_players(symbol,auto_symbol)
+    two_players(tc.colored(symbol,color="green"),auto_symbol)
 else:
-    auto_player(symbol,auto_symbol)
+    auto_player(tc.colored(symbol,color="green"),auto_symbol)
